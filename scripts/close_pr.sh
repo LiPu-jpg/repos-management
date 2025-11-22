431506aaf0c374c574f413c727c72f65e55ee4d1
Kowyo <kowyo@outlook.com>
2025-11-22 17:26:08 +0800

    feat: add close pr

---
+++ scripts/close_pr.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# List of repositories
+REPOS=$(cat repos_list.txt) # repos_list.txt 需要和本脚本在同一目录下
+
+# Define PR marker
+PR_MARKER="[automated-generated-pr]"
+
+for REPO in $REPOS; do
+  # Fetch number of the PR with '[automated-generated-pr]' in the title
+  # By default, fetch the number of the latest PR
+  PR_NUMBER=$(gh pr list -R "HITSZ-OpenAuto/$REPO" --search "${PR_MARKER} in:title" --json number -q '.[0].number')
+
+  if [ -z "$PR_NUMBER" ]; then
+    echo "No open pull requests found for $REPO"
+    continue
+  fi
+
+  # Close the pull request and delete the branch
+  gh pr close -R "HITSZ-OpenAuto/$REPO" "$PR_NUMBER" --delete-branch
+
+  echo "PR closed and branch deleted for $REPO"
+done
